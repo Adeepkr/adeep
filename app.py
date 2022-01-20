6e88aa2caeaebd5bb246adfe6f009897f4ca2dc8
Adeepkr <95479274+Adeepkr@users.noreply.github.com>
2022-01-20 17:05:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-
+import logging
 app = Flask(__name__)
+logging.basicConfig(filename='akr.log', level=logging.INFO)
 @app.route('/')
 def hello():
     return 'hello world adeep!'
 
 if __name__== "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80
